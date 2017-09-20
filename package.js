5be523e7fd215902d8de67b9c0ab6e46f84e54f2
Cleanderson Lobo <cleandersonlobo@gmail.com>
2017-09-20 12:32:42 -0300

    Update source to 6.9.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cleandersonlobo:sweetalert2',
-  version: '1.3.0',
+  version: '1.4.0',
   // Brief, one-line summary of the package.
   summary: 'Meteor Package for sweetAlert2: a beautiful replacement for javascript\' alert()',
   // URL to the Git repository containing the source code for this package.
